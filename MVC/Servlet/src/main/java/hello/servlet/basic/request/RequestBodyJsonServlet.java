37a0de763a661b8f985319d1902e7414bf831f45
kim <zabcd121@naver.com>
2022-07-18 20:23:13 +0900

    httl GET, POST HTML Form, text 실습

---
+++ MVC/Servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,8 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "requestBodyJsonServlet")
+public class RequestBodyJsonServlet extends HttpServlet {
+}
